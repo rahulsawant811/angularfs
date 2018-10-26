99750b980f99e5b8ea3fdba046844e9a26e11cf3
rahulsawant811 <rahulsawant811@gmail.com>
2018-10-26 13:49:50 +0530

    Changed the env settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDiYXsDVKAQbNdMQZgNTvaMHah1VZWSP50',
-    authDomain: 'fir-app-6a89b.firebaseapp.com',
-    databaseURL: 'https://fir-app-6a89b.firebaseio.com',
-    projectId: 'fir-app-6a89b',
-    storageBucket: 'fir-app-6a89b.appspot.com',
-    messagingSenderId: '621594967457'
+    apiKey: 'APIKEY',
+    authDomain: 'DOMAIN',
+    databaseURL: 'DB-URL',
+    projectId: 'PROJECT-ID',
+    storageBucket: 'BUCKET',
+    messagingSenderId: 'MSG-SENDER-ID'
   }
 };
